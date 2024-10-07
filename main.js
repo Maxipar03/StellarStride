785c605e867809f8b2457bc4f9e5874670844f4a
Maxipar03 <maxipar2003@gmail.com>
2024-10-07 01:59:07 -0300

    adding plans and contact section home

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-const burgerMenu = document.querySelector('.burgerMenu');
-const navMenu = document.querySelector('nav');
+document.querySelectorAll('.optionPlans').forEach(option => {
+    option.addEventListener('click', function() {
 
-burgerMenu.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
+        document.querySelectorAll('.optionPlans').forEach(op => op.classList.remove('selected'));
+        this.classList.add('selected');
+
+        const selectedPlan = this.getAttribute('data-plan');
+
+        document.querySelectorAll('.cardPlan').forEach(card => {
+            if (card.getAttribute('data-plan') === selectedPlan) {
+                card.classList.add('active');
+            } else {
+                card.classList.remove('active');
+            }
+        });
+    });
 });
